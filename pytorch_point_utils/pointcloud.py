Changes: - fix to pointcloud.knn() - adding get_random_translation, rotation, transform functions

pytorch_point_utils/pointcloud.py
@@ -146,7 +146,7 @@ def knn(src, tgt, k) -> torch.tensor:
     src = src.unsqueeze(0) if src.dim() == 2 else src
     tgt = tgt.unsqueeze(0) if tgt.dim() == 2 else tgt
     
-    dist = src.unsqueeze(-1) - tgt.unsqueeze(-2) # (..., N, M, C)
+    dist = src.unsqueeze(-2) - tgt.unsqueeze(-3) # (..., N, M, C)
     dist = torch.sum(dist ** 2, dim=-1) # (..., N, M)
     val, idx = dist.topk(k, dim=-1, largest=False) # (..., N, k)
     

pytorch_point_utils/transform.py
@@ -91,11 +91,11 @@ def compose(trans: torch.tensor, rot:torch.tensor) -> torch.Tensor:
     tf : torch.tensor # (..., 4, 4)
     """
     
-    tf = torch.matmul(rot, trans)
+    tf = torch.matmul(trans, rot)
     
     return tf
 
-def rotation_from_vector(v):
+def rotation_from_vector(v: torch.Tensor) -> torch.Tensor:
     """
     Convert a vector to a rotation matrix using Rodrigues' formula
 
@@ -128,3 +128,70 @@ def rotation_from_vector(v):
     rot_out = rot_out.squeeze(0) if one_dim else rot_out
     
     return rot_out
+
+def get_random_rotation(shape=None, device='cpu', dtype=torch.float32) -> torch.Tensor:
+    """
+    Generate a random rotation matrix
+
+    Parameters
+    ----------
+    shape : tuple, optional
+    device : str, optional
+    dtype : torch.dtype, optional
+
+    Returns
+    -------
+    rot : torch.Tensor # (..., 4, 4)
+    """
+    if shape is None:
+        rot = torch.rand(3, device=device, dtype=dtype)*3.14
+    else:
+        rot = torch.randn(*shape, 3, device=device, dtype=dtype)*3.14
+    
+    rot = rotation_from_vector(rot)
+    return rot
+
+def get_random_translation(shape=None, device='cpu', dtype=torch.float32, translation_range=1.0) -> torch.Tensor:
+    """
+    Generate a random translation matrix
+
+    Parameters
+    ----------
+    shape : tuple, optional
+    device : str, optional
+    dtype : torch.dtype, optional
+    translation_range : float, optional
+    
+    Returns
+    -------
+    trans : torch.Tensor # (..., 4, 4)
+    """
+    
+    if shape is None:
+        trans = torch.eye(4, device=device, dtype=dtype)
+        trans[:3, 3] = torch.randn(3, device=device, dtype=dtype)*translation_range
+    else:
+        shape = shape if isinstance(shape, tuple) else (shape,)
+        trans = batch_eye([*shape, 4], device=device, dtype=dtype)
+        trans[..., :3, 3] = torch.randn(*shape, 3, device=device, dtype=dtype)*translation_range
+        
+    return trans
+
+def get_random_transform(shape=None, device='cpu', dtype=torch.float32, translation_range=1.0) -> torch.Tensor:
+    """
+    Generate a random transformation matrix
+
+    Parameters
+    ----------
+    shape : tuple, optional
+    device : str, optional
+    dtype : torch.dtype, optional
+    translation_range : float, optional
+    
+    Returns
+    -------
+    tf : torch.Tensor # (..., 4, 4)
+    """
+    tf = torch.matmul(get_random_translation(shape, device, dtype, translation_range), 
+                      get_random_rotation(shape, device, dtype))
+    return tf